7119af0497831ca50aa53493b0b80d43e5693e80
shadowfaz16 <erikfazju@gmail.com>
2022-02-01 12:08:32 -0600

    toggle tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'go swimming',
+        day: 'Feb 1st at 8am',
+        reminder: false,
+        category: 'health'
+    },
+    {
+        id: 2,
+        text: 'Blockchain 101 course',
+        day: 'Feb 3rd at 8am',
+        reminder: true,
+        category: 'learning'
+    },
+    {
+        id: 3,
+        text: 'React course',
+        day: 'Feb 2nd at 8am',
+        reminder: false,
+        category: 'learning'
+    },
+])
+
+// delete task 
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+  console.log("deleted task ", id)
+}
+
+//toggle reminder
+const toggleReminder = (id) => {
+  console.log(id)
+}
+
   return (
     <div className="container">
       <Header title='Hello' />
-      <Tasks />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'no tasks'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,10 @@
-import { useState } from 'react'
+import Task from "./Task";
 
-const Tasks = () => {
-  const [tasks, setTasks] = usaState([
-    {
-        id: 1,
-        text: 'go swimming',
-        day: 'Feb 1st at 8am',
-        reminder: false,
-        category: 'health'
-    },
-    {
-        id: 2,
-        text: 'Blockchain 101 course',
-        day: 'Feb 3rd at 8am',
-        reminder: true,
-        category: 'learning'
-    },
-    {
-        id: 3,
-        text: 'React course',
-        day: 'Feb 2nd at 8am',
-        reminder: false,
-        category: 'learning'
-    },
-])
-
+const Tasks = ({ tasks, onDelete }) => {
   return (
   <>
-    {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+    {tasks.map((task) => (<Task key={task.id} task={task} onDelete={onDelete}/>
+    ))}
   </>
   )
 }
